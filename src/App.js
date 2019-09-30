e054636a7d1d469fdd2b070959f3460823925acb
Matt Herich <mjherich@gmail.com>
2019-09-30 14:49:12 -0700

    Add base layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
+      <div className="portfolio-grid">
+        <div className="portfolio-nav">
+          <h2>Matt Herich</h2>
+          <span>Full Stack Engineer</span>
+          <div className="nav-links">
+            <ul className="portfolio-links">
+              <li>Skills</li>
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+        <div className="main">
+          Main content
+        </div>
+      </div>
     </div>
   );
 }
