48dd244a87f75af76f732533a6c2bd777bd105ca
Matt Herich <mjherich@gmail.com>
2019-09-30 16:09:57 -0700

    Add fonts and update nav links

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -4,19 +4,17 @@ import './navigation.scss';
 export default function Navigation() {
   return (
     <div className="portfolio-nav">
-      {/* <div className="nav-container"> */}
-        <div className="site-title">
-          <h2>Matt Herich</h2>
-          <span>Full Stack Engineer</span>
-        </div>
-        <div className="nav-links">
-          <ul className="portfolio-links">
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-      {/* </div> */}
+      <div className="site-title">
+        <h2>Matt Herich</h2>
+        <span>Full Stack Engineer</span>
+      </div>
+      <div className="nav-links">
+        <ul className="portfolio-links">
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
