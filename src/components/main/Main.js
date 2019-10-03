65cb4e734bd080d1a0505557a49177b5b591b056
Matt Herich <mjherich@gmail.com>
2019-10-02 22:24:30 -0700

    Add skills (to be styled)

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -12,7 +12,18 @@ export default function Main() {
       </section>
       <section id="skills">
         <h2 className="section-title">Technical Skills</h2>
-        <p>List skills here...</p>
+        <h3>Core Competencies</h3>
+        <ul className="skills-list">
+          <li>HTML</li>
+          <li>CSS/LESS/SASS</li>
+          <li>Javascript</li>
+          <li>React JS</li>
+          <li>Java Spring</li>
+        </ul>
+        <h3>Some Experience</h3>
+        <ul className="skills-list">
+          <li>Ruby on Rails</li>
+        </ul>
       </section>
       <section id="projects">
         <h2 className="section-title">Projects</h2>
