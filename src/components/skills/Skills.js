30b793863f9492b94b88552944b1f5fa95fa58f9
Matt Herich <mjherich@gmail.com>
2020-03-30 18:50:02 -0700

    Break out sections into components

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <h2 className="section-title">Technical Skills</h2>
+      <h3>Core Competencies</h3>
+      <ul className="skills-list">
+        <li>HTML</li>
+        <li>CSS/LESS/SASS</li>
+        <li>Javascript</li>
+        <li>React JS</li>
+        <li>Java Spring</li>
+      </ul>
+      <h3>Some Experience</h3>
+      <ul className="skills-list">
+        <li>Ruby on Rails</li>
+      </ul>
+    </section>
+  )
+}
---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <section id="welcome">
+      <h2 className="section-title">Welcome,</h2>
+      <p>My name is Matt and I'm a full stack engineer who likes making cool stuff with code.</p>
+      <p>I'm currently attending <a href="https://lambdaschool.com/" target="_blank" rel="noopener noreferrer">Lambda School</a>, an intensive coding bootcamp designed to give students the skills necessary for working effectively on a software development team.</p>
+      <p>Seeking new opportunities Spring 2020.</p>
+    </section>
+  )
+}
