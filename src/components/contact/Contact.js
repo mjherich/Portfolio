5c495989b5c37e40ad3f5253d9a73ef02b96499a
Matt Herich <mjherich@gmail.com>
2020-04-06 18:11:28 -0700

    Add email to contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,8 +5,8 @@ export default function Contact() {
   return (
     <section id="contact">
       <Typography variant="h2"><span role="img" aria-label="email icon">📧</span> Contact</Typography>
-      <Typography variant="body1">
-        Form coming soon...
+      <Typography variant="body1" style={{marginTop: 35}}>
+        Form coming soon...<br/><br/>In the mean time email me: <a href="mailto:mjherich@gmail.com">mjherich@gmail.com</a>
       </Typography>
     </section>
   )
